366500622a5de4aa0061878a0ce2d8caee1eb21b
martyna2m <bsom2m@gmail.com>
2023-11-02 09:38:18 +0100

    Corrections after review

--- src/test/java/DriverSetup.java
+++ src/test/java/DriverSetup.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverSetup {
 
-    WebDriver driver;
+    public WebDriver driver;
 
     @BeforeAll
     static void setDriver() {
--- src/test/java/WebPageTitleTest.java
+++ src/test/java/WebPageTitleTest.java
@@ -4,50 +4,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class WebPageTitleTest extends DriverSetup {
 
-
     @ParameterizedTest
-    @CsvSource({"https://sii.pl/, 'Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska'"})
+    @CsvSource({"https://sii.pl/, 'Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska'",
+            "https://www.onet.pl/, Onet – Jesteś na bieżąco",
+            "http://kotuszkowo.pl/, Kotuszkowo- blog o kotach",
+            "https://www.filmweb.pl/, Filmweb - filmy takie jak Ty!",
+            "https://www.selenium.dev/documentation/en/webdriver/, WebDriver | Selenium"})
     @Tag("regression")
-    @Tag("sii")
-    void checkSiiWebsiteTitle(String url, String expectedTitle) {
+    void checkWebsiteTitle(String url, String expectedTitle) {
         WebPageTitleMethods.assertWebTitle(url, expectedTitle, driver);
     }
-
-    @ParameterizedTest
-    @CsvSource({"https://www.onet.pl/, Onet – Jesteś na bieżąco"})
-    @Tag("regression")
-    @Tag("onet")
-    void checkOnetWebsiteTitle(String url, String expectedTitle) {
-        WebPageTitleMethods.assertWebTitle(url, expectedTitle, driver);
-
-    }
-
-    @ParameterizedTest
-    @CsvSource({"http://kotuszkowo.pl/, Kotuszkowo- blog o kotach"})
-    @Tag("regression")
-    @Tag("cat")
-    void checkCatWebsiteTitle(String url, String expectedTitle) {
-        WebPageTitleMethods.assertWebTitle(url, expectedTitle, driver);
-
-    }
-
-    @ParameterizedTest
-    @CsvSource({"https://www.filmweb.pl/, Filmweb - filmy takie jak Ty!"})
-    @Tag("regression")
-    @Tag("filmweb")
-    void checkFilmWebsiteTitle(String url, String expectedTitle) {
-        WebPageTitleMethods.assertWebTitle(url, expectedTitle, driver);
-
-    }
-
-    @ParameterizedTest
-    @CsvSource({"https://www.selenium.dev/documentation/en/webdriver/, WebDriver | Selenium"})
-    @Tag("regression")
-    @Tag("selenium")
-    void checkSeleniumWebsiteTitle(String url, String expectedTitle) {
-        WebPageTitleMethods.assertWebTitle(url, expectedTitle, driver);
-
-    }
-
-
 }
